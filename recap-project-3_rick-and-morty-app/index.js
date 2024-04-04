9c992c8b7f67debb08f2e7a95e5c4732fdc41430
Davide Esposito <davideesposito2911@gmail.com>
2024-04-04 09:41:25 +0200

    pagination task

--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -11,14 +11,30 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters();
+  }
+});
+
 async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${page}`
+  );
   const data = await response.json();
-  console.log(data.results[0]);
+  // console.log(data.info.pages);
 
   cardContainer.innerHTML = "";
 
@@ -26,5 +42,10 @@ async function fetchCharacters() {
     const card = CharacterCard(character);
     cardContainer.appendChild(card);
   });
+
+  maxPage = data.info.pages;
+
+  pagination.textContent = `${page} / ${maxPage}`;
 }
+
 fetchCharacters();
